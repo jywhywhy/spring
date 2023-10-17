05a6f4e77ae5b23e0b0fc782b7f1869d0b5501cf
song <jefkdjsf2@naver.com>
2023-10-17 23:43:02 +0900

    [섹션6] 스프링 DB 접근 기술

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -8,7 +8,6 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username")
     private String name;
 
     public Long getId() {
