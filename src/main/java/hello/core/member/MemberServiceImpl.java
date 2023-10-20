d697fb69ef04aae9c964f8b025c55d6b2d924996
song <jefkdjsf2@naver.com>
2023-10-20 17:28:20 +0900

    [섹션3] 스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // 추상화에도 의존하고 구체화에도 의존(DIP위반)
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
