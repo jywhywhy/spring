eaac059e96e89e8a506c88c8d30ed8e6675d7b26
song <jefkdjsf2@naver.com>
2023-10-25 20:24:56 +0900

    [섹션3] 스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AppConfig에 있는 정보를 가지고 @Bean이 붙어있는 것들을 스프링 컨테이너에서 관리해줌
+        // ApplicationContext를 스프링 컨테이너라 한다
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig 찾을 Bean의 메서드 이름, 두번째는 타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
